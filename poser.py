22987fc714edc6b5ef042f81b711088ea6b57777
Ankur Taxali <ataxali@gmail.com>
2025-09-10 02:09:56 -0400

    Initial commit: Add poser.py and comprehensive .gitignore

---
+++ poser.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Poser - A simple Python library
+"""
+
+def main():
+    """Main entry point when running the script directly"""
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
