f1a80f1e4577e7c43ae65b79685029e615a9463d
Ankur Taxali <ataxali@gmail.com>
2025-09-28 15:03:16 -0400

    Remove Homepage, ScriptEditor, ToolsPanel, and TopBar components along with their associated styles to streamline the codebase and eliminate unused features.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
-  import HomePage from '$lib/components/pages/home/HomePage/Homepage.svelte';
+  import styles from './+page.module.scss';
+  import TopBar from '$lib/components/topBar/TopBar.svelte';
+  import ScriptEditor from '$lib/components/scriptEditor/ScriptEditor.svelte';
+  import ToolsPanel from '$lib/components/toolsPanel/ToolsPanel.svelte';
+  
 </script>
 
-<HomePage />
+<div class="{styles.pageContainer} {styles.dark}">
+  <div class={styles.container}>
+    <!-- Top Bar -->
+    <TopBar />
+
+    <!-- Content Container -->
+    <div class={styles.contentContainer}>
+      <!-- Left Column - Script Editor (3/4 width) -->
+      <ScriptEditor />
+
+      <!-- Right Column - Editing Tools (1/4 width) -->
+      <ToolsPanel />
+    </div>
+  </div>
+</div>
